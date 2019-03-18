f9898b73618aaac1cc2f62370d7d430df18c0a73
Anthony Lam <lamah314@gmail.com>
2019-03-18 14:42:53 -0400

    fixed mappings

--- src/main/java/org/wecancodeit/albumcollection/models/Album.java
+++ src/main/java/org/wecancodeit/albumcollection/models/Album.java
@@ -5,7 +5,12 @@ import java.util.Collection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+import org.assertj.core.util.Arrays;
 
 @Entity
 public class Album {
@@ -13,28 +18,47 @@ public class Album {
 	@Id
 	@GeneratedValue
 	private Long id;
-	
-	private String title;
-	
-	@ManyToMany(mappedBy="songs")
-	public Collection<Song> getSongs() {
-		return songs;
-	}
-	
-	private String image;
-	
-	private Collection<Song> songs;
-	
-	private String recordLabel;
-	
-	public Album () {}
 
-	public Album(String title, String image, Collection<Song> songs, String recordLabel) {
+	private String title;
+
+	private String image;
+
+	private String recordLabel;
+
+	@ManyToMany(mappedBy = "albums")
+	private Collection<Song> songs;
+
+	@ManyToMany
+	@JoinTable(name = "album_artist", joinColumns = { @JoinColumn(name = "album") }, inverseJoinColumns = {
+			@JoinColumn(name = "artist") })
+	private Collection<Artist> artists;
+
+	@OneToMany(mappedBy = "entity")
+	private Collection<Rating> ratings;
+
+	@OneToMany(mappedBy = "entity")
+	private Collection<Comment> comments;
+
+	@ManyToMany
+	@JoinTable(name = "album_tag", joinColumns = { @JoinColumn(name = "album") }, inverseJoinColumns = {
+			@JoinColumn(name = "tag") })
+	private Collection<Tag> tags;
+
+	public Album() {
+	}
+
+	public Album(String title, String image, String recordLabel, Song song, Artist artist, Rating rating,
+			Comment comment, Tag tag) {
 		super();
 		this.title = title;
 		this.image = image;
-		this.songs = songs;
 		this.recordLabel = recordLabel;
+		this.songs = Arrays.asList(song);
+		this.artists = Arrays.asList(artist);
+		this.ratings = Arrays.asList(rating);
+		this.comments = Arrays.asList(comment);
+		this.tags = Arrays.asList(tag);
+
 	}
 
 	public Long getId() {
@@ -48,15 +72,31 @@ public class Album {
 	public String getImage() {
 		return image;
 	}
-	
-	
+
 	public String getRecordLabel() {
 		return recordLabel;
 	}
-	
+
+	public Collection<Song> getSongs() {
+		return songs;
+	}
+
+	public Collection<Rating> getRatings() {
+		return ratings;
+	}
+
+	public Collection<Comment> getComments() {
+		return comments;
+	}
+
+	public Collection<Tag> getTags() {
+		return tags;
+	}
+
 	@Override
 	public String toString() {
-		return "Album [id=" + id + ", title=" + title + ", image=" + image + ", recordLabel=" + recordLabel + "]";
+		return "Album [id=" + id + ", title=" + title + ", image=" + image + ", recordLabel=" + recordLabel + ", songs="
+				+ songs + ", artists=" + artists + "]";
 	}
-	
+
 }

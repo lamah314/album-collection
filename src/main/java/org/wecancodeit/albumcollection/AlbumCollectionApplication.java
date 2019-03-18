56cd5d69fce55949f310c3b7ebbe1851e977cf07
Anthony Lam <lamah314@gmail.com>
2019-03-18 11:23:04 -0400

    initial commit

---
+++ src/main/java/org/wecancodeit/albumcollection/AlbumCollectionApplication.java
@@ -0,0 +1,13 @@
+package org.wecancodeit.albumcollection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbumCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbumCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wecancodeit/albumcollection/AlbumCollectionApplicationTests.java
@@ -0,0 +1,16 @@
+package org.wecancodeit.albumcollection;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlbumCollectionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

f2a78f6e17b6a3ca017edbb8a843e065e5370ecb
Lindsey Edwards <ledwards614@gmail.com>
2019-03-19 14:14:22 -0400

    added artist, song, and albums to print in javascript

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -11,10 +11,40 @@ fetch('/songs')
         
         let content = `<ul>`
         songs.forEach((song) => {
-            content += `<li>${song.title} </li>`;
+            content += `<li>TITLE: ${song.title} LINK: ${song.link} DURATION: ${song.duration} </li>`;
+            
         })
         content += `</ul>`;
 
         app.innerHTML = content;
     })
+    
+ const artist = document.querySelector("#artist");
+    fetch('/artists')
+    .then(response => response.json())
+    .then(artists => {
+        
+        let contentArtist = `<ul>`
+        artists.forEach((artist) => {
+            contentArtist += `<li>NAME: ${artist.name} IMAGE: ${artist.image} </li>`;
+        })
+        contentArtist += `</ul>`;
+
+        artist.innerHTML = contentArtist;
+    })
+    
+    const album = document.querySelector("#album");
+    fetch('/albums')
+    .then(response => response.json())
+    .then(albums => {
+        
+        let contentAlbums = `<ul>`
+        albums.forEach((album) => {
+            contentAlbums += `<li>NAME: ${album.title} IMAGE: ${album.image} ${album.recordLabel}</li>`;
+        })
+        contentAlbums += `</ul>`;
+
+        album.innerHTML = contentAlbums;
+    })
+    
     .catch(err => console.log(err))

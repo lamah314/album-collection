d61e50c9f631f270e1f55cd3ea4a3d15d0a8f4b6
JimJewell <jimhjewell@gmail.com>
2019-03-19 14:48:51 -0400

    populator update

--- src/main/java/org/wecancodeit/albumcollection/Populator.java
+++ src/main/java/org/wecancodeit/albumcollection/Populator.java
@@ -41,13 +41,24 @@ public class Populator implements CommandLineRunner {
 		Rating rating1 = ratingRepo.save(new Rating(5));
 		
 		Comment comment1 = commentRepo.save(new Comment("Great"));
+		Comment comment2 = commentRepo.save(new Comment("Sucks"));
+		Comment comment3 = commentRepo.save(new Comment("Epic"));
 		
 		Tag tag1 = tagRepo.save(new Tag("Cool"));
 		
-		Artist artist1 = artistRepo.save(new Artist("Anthony","MyFace.jpg"));
+		Artist artist1 = artistRepo.save(new Artist("Nirvana","nirvanaBand img"));
+		Album album1 = albumRepo.save(new Album("Nevermind", "albumNevermind.img", "DGC", artist1, rating1, comment3, tag1));
+		Song song1 = songRepo.save(new Song("Lithium", "https://www.youtube.com/watch?v=pkcJEvMcnEg", "4:16", artist1, album1));	
+		Song song2 = songRepo.save(new Song("Heart Shaped Box", "https://www.youtube.com/watch?v=n6P0SitRwy8", "4:45", artist1, album1));	
 		
-		Album album1 = albumRepo.save(new Album("Circle", "Square", "Triangle", artist1, rating1, comment1, tag1));
-
-		Song song1 = songRepo.save(new Song("Singing", "LINK", "duration", artist1, album1));	
+		Artist artist2 = artistRepo.save(new Artist("Nine Inch Nails","NineInchNails img"));
+		Album album2 = albumRepo.save(new Album("Pretty Hate Machine", "albumPrettyHateMachine.img", "TVT", artist2, rating1, comment3, tag1));
+		Song song3 = songRepo.save(new Song("Down In It", "https://www.youtube.com/watch?v=NkphW38WyYo", "4:37", artist2, album2));	
+		Song song4 = songRepo.save(new Song("Head Like A Hole", "https://www.youtube.com/watch?v=nOVW938sr0k", "5:00", artist2, album2));
+		
+		Artist artist3 = artistRepo.save(new Artist("Sublime","Sublime img"));
+		Album album3 = albumRepo.save(new Album("Sublime", "albumSublime.img", "MCA", artist3, rating1, comment3, tag1));
+		Song song5 = songRepo.save(new Song("Santeria", "https://www.youtube.com/watch?v=k3ZeUL4fRyko", "3:04", artist3, album3));	
+		Song song6 = songRepo.save(new Song("What I got", "https://www.youtube.com/watch?v=0Uc3ZrmhDN4", "2:53", artist3, album3));
 	}
 }

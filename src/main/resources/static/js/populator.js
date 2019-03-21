db75335736543a8b30e74a651be8d618591ccc81
Anthony Lam <lamah314@gmail.com>
2019-03-21 15:26:01 -0400

    fixed things up

--- src/main/resources/static/js/populator.js
+++ src/main/resources/static/js/populator.js
@@ -1,6 +1,6 @@
 let newArtist = {
     name: "Red Hot Chili Peppers",
-    image: "Red Hot Chili Peppers img"
+    image: "Red Hot Chili Peppers image"
 }
 
 // postData(`/artists/addArtist`, {
@@ -10,18 +10,18 @@ let newArtist = {
 //     .then(data => console.log(data))
 //     .catch(err => console.log(err))
 
-function postArtistData(url = ``, data = {}) {
-    // Default options are marked with *
-    return fetch(url, {
-            method: "POST", // *GET, POST, PUT, DELETE, etc.
-            headers: {
-                "Content-Type": "text/plain",
-                // "Content-Type": "application/x-www-form-urlencoded",
-            },
-            body: data, // body data type must match "Content-Type" header
-        })
-        .then(response => response.json()); // parses JSON response into native Javascript objects 
-}
+// function postArtistData(url = ``, data = {}) {
+//     // Default options are marked with *
+//     return fetch(url, {
+//             method: "POST", // *GET, POST, PUT, DELETE, etc.
+//             headers: {
+//                 "Content-Type": "text/plain",
+//                 // "Content-Type": "application/x-www-form-urlencoded",
+//             },
+//             body: data, // body data type must match "Content-Type" header
+//         })
+//         .then(response => response.json()); // parses JSON response into native Javascript objects 
+// }
 
 fetch("/artists/addArtist", {
         method: "POST", // *GET, POST, PUT, DELETE, etc.

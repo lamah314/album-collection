5483b674eec5d3fca1101a3b6e1d2240769f3a95
Anthony Lam <lamah314@gmail.com>
2019-03-26 10:02:34 -0400

    waiting for workshop

--- src/main/resources/static/js/Utils/API/api-actions.js
+++ src/main/resources/static/js/Utils/API/api-actions.js
@@ -1,3 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor() {
+      super();
+    }
+    increment = () => {
+      this.setState({count: this.state.count + 1 });
+    }
+    decrement = () => {
+      this.setState({count: this.state.count - 1 });
+    }
+
+
 function getRequest(location, callback) {
     fetch(location)
         .then(response => response.json())
